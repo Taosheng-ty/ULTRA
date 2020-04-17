efeb3934a5d368af1db505be43e9a0d8ba7f87a1
Taosheng-ty <taoyang@cs.utah.edu>
2020-04-17 09:56:58 -0600

    change DNN normalization method to layer norm instead of batch norm because it performs much better

--- learning_algorithm/ranking_model/DNN.py
+++ learning_algorithm/ranking_model/DNN.py
@@ -20,7 +20,10 @@ class DNN(BasicRankingModel):
         self.initializer = None
         if self.hparams.initializer == 'constant':
             self.initializer = tf.constant_initializer(0.001)
-
+        output_sizes = self.hparams.hidden_layer_sizes + [1]
+        self.layer_norm=[]
+        for j in range(len(output_sizes)):
+            self.layer_norm.append(tf.keras.layers.LayerNormalization(name="DNN_layer_norm_%d"%j))
     def build(self, input_list, is_training=False):
         """ Create the model
         
@@ -36,22 +39,31 @@ class DNN(BasicRankingModel):
         with tf.variable_scope(tf.get_variable_scope(), initializer=self.initializer,
                                             reuse=tf.AUTO_REUSE):
             input_data = tf.concat(input_list, axis=0)
-            output_data = tf.compat.v1.layers.batch_normalization(input_data, training=is_training, name="input_batch_normalization")
+#             output_data = tf.compat.v1.layers.batch_normalization(input_data, training=is_training, name="input_batch_normalization")
+            output_data = input_data
             output_sizes = self.hparams.hidden_layer_sizes + [1]
             current_size = output_data.get_shape()[-1].value
             for j in range(len(output_sizes)):
+#                 output_data = tf.compat.v1.layers.batch_normalization(output_data, training=is_training, name="batch_normalization_%d" % j)
+                output_data=self.layer_norm[j](output_data)
                 expand_W = tf.get_variable("dnn_W_%d" % j, [current_size, output_sizes[j]]) 
                 expand_b = tf.get_variable("dnn_b_%d" % j, [output_sizes[j]])
                 output_data = tf.nn.bias_add(tf.matmul(output_data, expand_W), expand_b)
-                output_data = tf.compat.v1.layers.batch_normalization(output_data, training=is_training, name="batch_normalization_%d" % j)
+
                 # Add activation if it is a hidden layer
                 if j != len(output_sizes)-1:
                     output_data = tf.nn.elu(output_data)
                 current_size = output_sizes[j]
-            output=tf.split(output_data, len(input_list), axis=0) 
-            print(len(output),output[0].get_shape(),"len(output),output[0].get_shape()in DNN")
+            output=tf.split(output_data, len(input_list), axis=0)
+            out=tf.concat(output,axis=1)
+            
+            out=tf.nn.softmax(out,axis=1)
+            output1=[]
+            for i in range(len(input_list)):
+                output1.append(tf.expand_dims(out[:,i],axis=1))
+            print(len(output1),output1[0].get_shape(),"len(output),output[0].get_shape()in DNN")
             ## output should be a list [len_seq], and each item should be (batch,1)
-            return output
+            return output1
     
     def build_with_random_noise(self, input_list, noise_rate, is_training=False):
         """ Create the model
