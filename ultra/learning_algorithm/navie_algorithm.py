7688e789e6418fe3d1d29223fb7b632dcfb18f19
Taosheng-ty <taoyang@cs.utah.edu>
2020-04-25 17:05:53 -0600

    minor debug no need to remove pad for training

--- ultra/learning_algorithm/navie_algorithm.py
+++ ultra/learning_algorithm/navie_algorithm.py
@@ -88,11 +88,11 @@ class NavieAlgorithm(BaseAlgorithm):
 
             self.loss = None
             if self.hparams.loss_func == 'sigmoid_cross_entropy':
-                self.loss = self.sigmoid_loss(pad_removed_train_output, reshaped_train_labels)
+                self.loss = self.sigmoid_loss(train_output, reshaped_train_labels)
             elif self.hparams.loss_func == 'pairwise_loss':
-                self.loss = self.pairwise_loss(pad_removed_train_output, reshaped_train_labels)
+                self.loss = self.pairwise_loss(train_output, reshaped_train_labels)
             else:
-                self.loss = self.softmax_loss(pad_removed_train_output, reshaped_train_labels)
+                self.loss = self.softmax_loss(train_output, reshaped_train_labels)
             params = tf.trainable_variables()
             if self.hparams.l2_loss > 0:
                 loss_l2 = 0.0
