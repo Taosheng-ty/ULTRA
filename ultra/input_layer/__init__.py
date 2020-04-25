e44005468e502e856f9d7d9b901b25b2077fde98
Taosheng-ty <taoyang@cs.utah.edu>
2020-04-24 22:06:17 -0600

    adapt for setrakn and dlcm

--- ultra/input_layer/__init__.py
+++ ultra/input_layer/__init__.py
@@ -1,12 +1,12 @@
 # note:
 from __future__ import absolute_import
-from .click_models import *
-from .ClickSimulationFeed import *
-from .DirectLabelFeed import *
+from .base_input_feed import *
+from .click_simulation_feed import *
+from .direct_label_feed import *
 from .DeterministicOnlineSimulationFeed import *
 from .StochasticOnlineSimulationFeed import *
 from .clicksimulation_with_type import *
-from .InterleavingDeterministicOnlineSimulationFeed import *
+# from .InterleavingDeterministicOnlineSimulationFeed import *
 from .DirectLabelFeed_with_clicks import *
 
 
