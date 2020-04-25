7785095795c82d8b3c75a08be38f6471d5c8c7df
Taosheng-ty <taoyang@cs.utah.edu>
2020-04-25 10:15:48 -0600

    move learninig and input to ultra

--- learning_algorithm/ranking_model/__init__.py
+++
@@ -1,8 +0,0 @@
-# note:
-from __future__ import absolute_import
-from .BasicRankingModel import *
-from .DNN import *
-from .Linear import *
-from .DLCM import *
-from .Transformer import *
-from .Setrank import *
